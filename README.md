a527d67e5604cf8cf2f2eb9de6c0b457cc1805dd
thinalfdo <thinalfernando1971@gmail.com>
2024-10-07 15:17:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Getting Started with React App
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+
+This will Install all the libraries required in the app.
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ src/config.js
@@ -0,0 +1,19 @@
+const CLIENT_ID = process.env.CLIENT_ID || '0oaj768gztoAaQ1Xh697';
+const ISSUER = process.env.ISSUER || 'https://trial-6040877.okta.com/oauth2/default';
+const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
+const REDIRECT_URI = `${window.location.origin}/login/callback`;
+
+// eslint-disable-next-line
+export default {
+  oidc: {
+    clientId: CLIENT_ID,
+    issuer: ISSUER,
+    redirectUri: REDIRECT_URI,
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
+  },
+  resourceServer: {
+    messagesUrl: 'http://localhost:8000/api/messages',
+  },
+};
